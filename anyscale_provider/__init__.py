1cc7e6b6a9ec0cc37cfb4a9419490fe5517c7eb7
Matias Lopez <34432076+mlopezgez@users.noreply.github.com>
2022-09-30 16:58:39 +0000

    __init__

--- anyscale_provider/__init__.py
+++ anyscale_provider/__init__.py
@@ -2,8 +2,6 @@ def get_provider_info():
     return {
         "package-name": "airflow-provider-anyscale",
         "name": "Anyscale Airflow Provider",
-        "description": "An Apache Airflow provider for Ray.",
-        "hook-class-names": [],
-        "extra-links": [],
+        "description": "An Apache Airflow provider for Anyscale.",
         "versions": ["0.0.1"]
     }
